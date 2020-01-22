b7e587e9a95f88044a450bba1a25ab5fe70dfd54
Janssen Kuhn <noconsulate@gmail.com>
2020-01-22 09:05:23 -0700

    formik

--- pages/fbtest.js
+++ pages/fbtest.js
@@ -1,54 +1,63 @@
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
+import React from 'react';
+import { Formik } from 'formik';
 
-import { Formik } from 'formik'
+const Basic = () => (
+  <div>
+    <h1>Anywhere in your app!</h1>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+  </div>
+);
 
-import {useEffect} from 'react'
-
-
-const Fbtest = props => {
-  useEffect(() => {
-    var firebaseConfig = {
-      apiKey: "AIzaSyDbC1jdZ8p9m6Wtx10vJh6rUAqRDQTbT3I",
-      authDomain: "empirical-seo.firebaseapp.com",
-      projectId: "empirical-seo",
-    };
-    firebase.initializeApp(firebaseConfig)
-    var db = firebase.firestore()
-    
-    db.collection('users').add({
-      first: 'ada',
-      last: 'lovelace',
-      born: 1815
-    })
-    .then(docRef => {
-      console.log('document written with id: ', docRef.id)
-    })
-    .catch(error => {
-      console.error('error: ', error)
-    })
-    
-    // Add a second document with a generated ID.
-    db.collection("users").add({
-      first: "Alan",
-      middle: "Mathison",
-      last: "Turing",
-      born: 1912
-    })
-    .then(function(docRef) {
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
-    
-  }, [])
-
-
-  return (
-    <p>firebase is cool</p>
-  )
-}
-
-export default Fbtest
+export default Basic;
--- pages/survey.js
+++ pages/survey.js
@@ -1,12 +1,41 @@
 import { useEffect } from 'react'
 import Typography from '@material-ui/core/Typography'
-import TextField from '@material-ui/core/TextField'
+import { Formik, Form, Field } from 'formik';
+import { TextField } from 'formik-material-ui';
 import Button from '@material-ui/core/Button'
 
 import Layout from '../components/Layout'
 
 import { db } from '../config/firebase'
 
+const MyForm = () => (
+  <Formik
+    initialValues={{ keywords: 'keywordz' }}
+    validate={values => {
+      // Your client-side validation logic
+    }}
+    onSubmit={(values, { setSubmitting }) => {
+      console.log(values.keywords)
+      alert(values.keywords)
+      setSubmitting(false)
+    }}
+  >
+    {({ isSubmitting }) => (
+      <Form>
+        <TextField name='keywords' />
+        <Button
+         type="submit"
+         fullWidth
+         variant="contained"
+         color="primary"
+         disabled={isSubmitting}
+       >
+         Submit
+       </Button>
+      </Form>
+    )}
+  </Formik>
+);
 
 const pageContent = (
   <div>
@@ -17,27 +46,12 @@ const pageContent = (
     <Typography variant='h5'>
       Find the best place to party Las Vegas
       </Typography>
-    <form noValidate autoComplete='off'>
-      <TextField label='keywords' /> <br />
-      <Button variant='contained'>submit</Button>
-    </form>
+    {MyForm()}
   </div>
 )
 
 const Survey = props => {
-  useEffect(() => {
-    db.collection('users').add({
-      first: 'ada',
-      last: 'lovelace',
-      born: 1815
-    })
-      .then(docRef => {
-        console.log('document written with id: ', docRef.id)
-      })
-      .catch(error => {
-        console.error('error: ', error)
-      })
-  }, [])
+  
   return (
     <Layout
       content={pageContent}
