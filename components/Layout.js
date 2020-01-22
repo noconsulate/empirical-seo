9e22e4283c8bf1ac17c175c6d5f1a8b23a195549
Janssen Kuhn <noconsulate@gmail.com>
2020-01-21 21:34:03 -0700

    layout integrated

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,16 @@
 import Typography from '@material-ui/core/Typography'
+import Head from 'next/head'
 
-import NavBar from './NavBar'
+import Header from './Header'
 
 const Layout = props => {
   return (
+
     <div>
-      <NavBar />
-      <Typography variant='h3'>
-        Empircal SEO
-      </Typography>
+      <Head>
+        <title>{props.title}</title>
+      </Head>
+      <Header />
       {props.content}
     </div>
   )
--- pages/survey.js
+++ pages/survey.js
@@ -3,18 +3,10 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Head from 'next/head'
 
-const Survey = props => {
+import Layout from '../components/Layout'
 
-  return (
-    
-    <div>
-      <Head>
-        <title>A Simple Survey</title>
-      </Head>
-      
-      <Typography variant='h3'>
-        Empirical SEO
-      </Typography>
+const pageContent = (
+<div>
       <Typography variant='body1'>
         this is a digital scavenger hunt. Top searchers you will be rewarded by being entered into the contest
       </Typography>
@@ -27,6 +19,16 @@ const Survey = props => {
         <Button variant='contained'>submit</Button>
       </form>
     </div>
+)
+
+const Survey = props => {
+
+  return (
+    <Layout
+      content={pageContent}
+      title='a Simple Survey'
+    />
+    
   )
 }
 
