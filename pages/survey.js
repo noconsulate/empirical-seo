9c34abecc9b299c6d3192369ab81bb002cda1af4
Janssen Kuhn <noconsulate@gmail.com>
2020-01-30 15:22:08 -0700

    refactor

--- pages/survey.js
+++ pages/survey.js
@@ -13,59 +13,61 @@ const FormSchema = Yup.object().shape({
     .required('The search field is empty!')
 })
 
-const MyForm = () => (
 
 
-  <Formik
-    initialValues={{ keywords: '' }}
-    validationSchema={FormSchema}
-    onSubmit={(values, { setSubmitting }) => {
-      const keywords = values.keywords.split(' ').filter(item => item != '')
-      console.log('keywords', keywords)
-      db.collection('keywords').add({
-        words: keywords
-      })
-      .then(docRef => {
-        console.log("Keywords written to: ", docRef.id)
-      })
-      .catch(error => {
-        console.error("Error adding document: ", docRef.id);
-      })
-      setSubmitting(false)
-    }}
-  >
-    {({ isSubmitting }) => (
-      <Form>
-        <TextField name='keywords' placeholder='keywords here' />
-        <ErrorMessage name='keywords' />
-        <Button
-         type="submit"
-         fullWidth
-         variant="contained"
-         color="primary"
-         disabled={isSubmitting}
-       >
-         Submit
-       </Button>
-      </Form>
-    )}
-  </Formik>
-);
 
-const pageContent = (
-  <div>
-    <Typography variant='body1'>
-      this is a digital scavenger hunt. Top searchers you will be rewarded by being entered into the contest
-      </Typography>
-    <br />
-    <Typography variant='h5'>
-      Find the best place to party Las Vegas
-      </Typography>
-    {MyForm()}
-  </div>
-)
 
 const Survey = props => {
+  const MyForm = () => (
+
+    <Formik
+      initialValues={{ keywords: '' }}
+      validationSchema={FormSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        const keywords = values.keywords.split(' ').filter(item => item != '')
+        console.log('keywords', keywords)
+        db.collection('keywords').add({
+          words: keywords
+        })
+        .then(docRef => {
+          console.log("Keywords written to: ", docRef.id)
+        })
+        .catch(error => {
+          console.error("Error adding document: ", docRef.id);
+        })
+        setSubmitting(false)
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <TextField name='keywords' placeholder='keywords here' />
+          <ErrorMessage name='keywords' />
+          <Button
+           type="submit"
+           fullWidth
+           variant="contained"
+           color="primary"
+           disabled={isSubmitting}
+         >
+           Submit
+         </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+  
+  const pageContent = (
+    <div>
+      <Typography variant='body1'>
+        this is a digital scavenger hunt. Top searchers you will be rewarded by being entered into the contest
+        </Typography>
+      <br />
+      <Typography variant='h5'>
+        Find the best place to party Las Vegas
+        </Typography>
+      {MyForm()}
+    </div>
+  )
   
   return (
     <Layout
