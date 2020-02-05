11726fc91223b21a1133600b8370d4eff9cf1f42
Janssen Kuhn <noconsulate@gmail.com>
2020-02-05 15:32:02 -0700

    bye bye clam clam

--- pages/survey.js
+++ pages/survey.js
@@ -99,7 +99,7 @@ const Survey = props => {
       >
         AUTHENTICATE!
       </Button>
-      <img src='https://firebasestorage.googleapis.com/v0/b/empirical-seo.appspot.com/o/photo_2020-01-30_16-07-10.jpg?alt=media&token=25358198-3148-425c-b57e-0d49f0e544b7' /> 
+      
     </>
   )
 
@@ -108,7 +108,7 @@ const Survey = props => {
       <Typography variant='body1'>
         Thank you for your participation. Hopefully it will be YOU on this beach with the lovely clam!
       </Typography>
-      <img src='https://firebasestorage.googleapis.com/v0/b/empirical-seo.appspot.com/o/photo_2020-01-30_16-07-10.jpg?alt=media&token=25358198-3148-425c-b57e-0d49f0e544b7' /> 
+      
     </>
   )
   const afterSubmit = () => (
