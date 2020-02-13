28e0c29fb1fc5efa64c48add57c8ebe2103593a6
Janssen Kuhn <noconsulate@gmail.com>
2020-02-13 09:22:26 -0700

    textfield losing focus, going to rewrite

--- pages/create.js
+++ pages/create.js
@@ -1,101 +1,93 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import {
-  Typography, Button, Grid, FormControl
+  Typography, Button, Grid, FormControl, TextField
 } from '@material-ui/core'
 import { Formik, Form, ErrorMessage } from 'formik';
-import { TextField } from 'formik-material-ui';
 
 import { db } from '../config/firebase'
 
 import Layout from '../components/Layout'
-import ScenarioForm from '../components/ScenarioForm'
 
 const useStyles = makeStyles(theme => ({
   extra: {
     backgroundColor: 'red',
   },
   form: {
-    backgroundColor: 'purple',
+    backgroundColor: 'yellow',
+  },
+  description: {
+    backgroundColor: 'orange',
   },
 }))
 
 const Create = props => {
   const [id, setId] = React.useState('')
   const [pageControl, setPageControl] = React.useState(0)
+  const [scenarioText, setScenarioText] = React.useState('')
+  const [test, setTest] = React.useState('')
 
   const classes = useStyles()
 
-  const MyScenarioForm = () => (
-    <Formik
-      initialValues={{ scenario: '' }}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(values)
+  const handleChangeScenario = event => {
+    console.log('keypress')
+    
+    setScenarioText(event.target.value)
+    const { target } = event;
 
-        db.collection('scenarios').add({
-          scenario: values.scenario,
-        })
-          .then(docRef => {
-            console.log('scenario written to: ', docRef.id)
-            setId(docRef.id)
-            setPageControl(1)
-          })
-          .catch(error => {
-            console.error('error adding document: ', error.message)
-            
-          })
+  }
 
-        setSubmitting(false)
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Grid container direction='column' className={classes.form}>
-          <Form>
-            <Grid item>
-              <TextField 
-                name='scenario' 
-                placeholder='scenario here' 
-                autoComplete='off' 
-                fullWidth
-              />
+  const handleSubmitScenario = event => {
+    event.preventDefault()
+    db.collection('scenarios').add({
+      scenario: scenarioText,
+    })
+      .then(docRef => {
+        console.log('scenario written to: ', docRef.id)
+        setId(docRef.id)
+        setPageControl(1)
+      })
+      .catch(error => {
+        console.error('error adding document: ', error.message)
 
-            </Grid>
-            
-            <Grid item>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                disabled={isSubmitting}
-              >
-                Submit
-              </Button>
-            </Grid>
+      })
+  }
 
-          </Form>
-        </Grid>
-      )}
-    </Formik>
-  );
+  const handleTest = event => {
+    setTest(event.target.value)
+  }
 
-  // const ScenarioForm = () => {
-  //   return (
-  //     <div className={classes.extra}>
-  //       <Typography variant='body1'>
-  //         Please consider a scenario that describes the situation your user will be in when they do a Google search that will lead them to your website.
-  //     </Typography>
-  //     {MyScenarioForm()}
-  //     </div>
-  //   )
-  // }
-
+  const Scenarioform = props => {
+    return (
+      <div>
+        <div className={classes.description}>
+          <Typography variant='body1'>
+            Please consider a scenario that describes the situation your user will be in when they do a Google search that will lead them to your website.
+          </Typography>
+        </div>
+        <form autoComplete='off' className={classes.form} onSubmit={handleSubmitScenario}>
+          <TextField 
+            label="scenario"
+            multiline
+            fullWidth
+            rowsMax='2'
+            value={scenarioText}
+            onChange={handleChangeScenario}
+          />
+          <TextField value={test} onChange={handleTest} />
+          <Button type='submit' variant='contained'>
+            Submit
+          </Button>
+        </form>
+      </div>
+    )
+  }
   const LoginForm = () => {
     return (
       <div className={classes.extra}>
         <Typography variant='body1'>
           In order for us to secure your results so that only you can see them, you need to make an account with us. Don't worry, we won't ever email you unless you opt in and we won't share your information with anyone!
         </Typography>
-        <div id='firebaseui'></div>
       </div>
     )
   }
@@ -104,7 +96,7 @@ const Create = props => {
     <>
       {
         {
-          0: <ScenarioForm />,
+          0: <Scenarioform />,
           1: <LoginForm />
         }[pageControl]
       }
