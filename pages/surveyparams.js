36f7b11dea5817eab1422fcb45a43bc229c488b6
Janssen Kuhn <noconsulate@gmail.com>
2020-02-14 14:17:11 -0700

    'broke the db get(), probably because i changed data structure for keywords collection?

--- pages/surveyparams.js
+++ pages/surveyparams.js
@@ -20,42 +20,57 @@ const useStyles = makeStyles(theme => ({
 
 const Survey = props => {
   const [urlId, setUrlId] = React.useState('')
+  const [scenarioId, setScenarioId] = React.useState('')
   const [scenarioText, setScenarioText] = React.useState('')
   const [formText, setFormText] = React.useState('')
 
   const classes = useStyles()
-  
+
   const router = useRouter()
   React.useEffect(() => {
     const value = router.query.urlid
     setUrlId(value)
     console.log(urlId)
-    
-  })
-
-  if (urlId !== '') {
-    const scenariosRef = db.collection('scenarios')
-    const query = scenariosRef.where('urlId', '==', urlId)
-    query.get()
-      .then(snapshot => {
-        snapshot.forEach(doc => {
-          console.log(doc.id, doc.data())
-          const value = doc.data().scenario
-          setScenarioText(value)
+    if (urlId) {
+      const scenariosRef = db.collection('scenarios')
+      const query = scenariosRef.where('urlId', '==', urlId)
+      query.get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            console.log(doc.id, doc.data())
+            const scenarioValue = doc.data().scenario
+            setScenarioText(scenarioValue)
+            setScenarioId(doc.id)
+          })
         })
-      })
-      .catch(error => {
-        console.log('error', error)
-      })
-  }
+        .catch(error => {
+          console.log('error', error)
+        })
+    }
+  }, [])
+
+
 
   const handleChange = event => {
-    event.preventDefault()
     setFormText(event.target.value)
   }
 
   const handleSubmit = event => {
-    console.log(formText)
+    event.preventDefault()
+    console.log('submit', formText)
+
+    const keywords = formText.split(' ').filter(item => item != '')
+    console.log('keywords', keywords)
+    db.collection('keywords').add({
+      words: keywords,
+      scenario: scenarioId,
+    })
+      .then(docRef => {
+        console.log("Keywords written to: ", docRef.id)
+      })
+      .catch(error => {
+        console.error("Error adding document: ", error.message);
+      })
   }
 
   const SurveyForm = () => {
@@ -63,24 +78,24 @@ const Survey = props => {
     return (
       <>
         <Grid container direction='column'>
-        <div className={classes.content}>
-          <Typography variant='body1'>
-            {scenarioText}
-          </Typography>
-        </div>
-        <form 
-          className={classes.form}
-          autoComplete='off'
-          onSubmit={handleSubmit}
-        >
-          <TextField
-            value={formText}
-            onChange={handleChange}
-          />
-          <Button type='submit'>
-          google!
-        </Button>
-        </form>
+          <div className={classes.content}>
+            <Typography variant='body1'>
+              {scenarioText}
+            </Typography>
+          </div>
+          <form
+            className={classes.form}
+            autoComplete='off'
+            onSubmit={handleSubmit}
+          >
+            <TextField
+              value={formText}
+              onChange={handleChange}
+            />
+            <Button type='submit'>
+              google!
+            </Button>
+          </form>
         </Grid>
       </>
     )
@@ -94,7 +109,7 @@ const Survey = props => {
 
   return (
     <>
-      <Layout 
+      <Layout
         content={pageContent}
         title={'SURVEY NEEDS TITLE YA KNOW'}
       />
