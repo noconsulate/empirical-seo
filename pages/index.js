76daa919f430f1dbb1b624954ae61fb8b83a0a9e
Janssen Kuhn <noconsulate@gmail.com>
2020-02-18 09:44:24 -0700

    path '/' forwards to '/create'

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,39 @@
-import Link from '../src/Link'
+// import Link from '../src/Link'
 
-import Layout from '../components/Layout'
+// import Layout from '../components/Layout'
 
-const pageContent = (
-  <div>
-    <Link href='/survey'>
-      Survey
-    </Link>
-  </div>
-)
+// const pageContent = (
+//   <div>
+//     <Link href='/survey'>
+//       Survey
+//     </Link>
+//   </div>
+// )
 
-const Index = () => {
+// const Index = () => {
+//   return (
+//     <Layout 
+//       content={pageContent} 
+//       title='Empirical SEO'
+//     />
+//   )
+// }
+
+// export default Index
+
+import React, {useEffect} from 'react';
+import Router from 'next/router'
+
+export default function App() {
+
+  useEffect(() => { 
+    const {pathname} = Router
+    if(pathname == '/' ){
+      Router.push('/create')
+    }  
+  }
+  , []);
   return (
-    <Layout 
-      content={pageContent} 
-      title='Empirical SEO'
-    />
+    null
   )
 }
-
-export default Index
