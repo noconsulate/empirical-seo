4de4d7a74c186b859c10b93da2f0ebdcf32cba51
Janssen Kuhn <noconsulate@gmail.com>
2020-02-28 09:55:02 -0700

    comments

--- pages/portal.js
+++ pages/portal.js
@@ -5,7 +5,7 @@ import {
 import { makeStyles } from '@material-ui/core/styles'
 
 import Link from '../src/Link'
-import { fbAuth, db, dbArrayUnion,} from '../config/firebase'
+import { fbAuth, db, dbArrayUnion, } from '../config/firebase'
 
 import PortalCreate from '../components/PortalCreate'
 import Layout from '../components/Layout'
@@ -36,6 +36,7 @@ const portal = props => {
 
   useEffect(() => {
     console.log(mode)
+    // auth and db operation for signin mode
     if (mode == 'signin') {
       const dbUpdate = () => {
         let docRef = db.collection('users').doc(uid)
@@ -44,7 +45,7 @@ const portal = props => {
             console.log('user exists')
             docRef.set({
               optIn: optIn,
-            }, {merge: true })
+            }, { merge: true })
           } else {
             console.log('user dont exists')
             docRef.set({
@@ -74,10 +75,10 @@ const portal = props => {
           })
           .catch(error => {
             console.log('error', error)
-          }) 
+          })
       }
       // firebase authentication
-      
+
       const userProcess = async () => {
         user = await fbAuth.currentUser
         if (user) {
@@ -111,84 +112,84 @@ const portal = props => {
       userProcess()
       console.log(scenarios)
     } else {
-       // update 'users' collection in database, get data for render d
-    const dbUpdate = () => {
-      let docRef = db.collection('users').doc(uid)
-      docRef.get().then(doc => {
-        if (doc.exists) {
-          console.log('user exists')
-          docRef.set({
-            optIn: optIn,
-            urlIds: dbArrayUnion(urlId)
-          }, {merge: true })
-        } else {
-          console.log('user dont exists')
-          docRef.set({
-            urlIds: [urlId],
-            email: userEmail,
-            optIn: optin
-          })
-        }
-        // get urlids for render **not working for some reason. maybe refactor to async function?
-        let urlsGet
-        db.collection('users').doc(uid).get()
-          .then(doc => {
-            urlsGet = doc.data().urlIds
-            console.log(urlsGet)
-          })
-          .then(doc => {
-            setScenarios(urlsGet)
-            console.log(scenarios)
-          })
-      })
-      // add user id to scenario  
-    //   db.collection('scenarios').doc(scenarioId).update({
-    //     "owner": uid,
-    //     "private": true,
-    //   })
-    //     .then(() => {
-    //       console.log('scenario uptdated')
-    //     })
-    //     .catch(error => {
-    //       console.log('error', error)
-    //     }) 
-     }
-    // firebase authentication
-    
-    const userProcess = async () => {
-      user = await fbAuth.currentUser
-      if (user) {
-        console.log('user already signed in', user)
-        uid = user.uid
-        userEmail = user.email
-        console.log(userEmail)
-        dbUpdate()
-      } else {
-        // if no user proceed with email link validation *
-        console.log('no user')
-        if (fbAuth.isSignInWithEmailLink(window.location.href)) {
-          let email = window.localStorage.getItem('emailForSignIn')
-          if (!email) {
-            email = window.prompt('please provide email for account confirmation')
+      // auth/db operations for create mode
+      const dbUpdate = () => {
+        let docRef = db.collection('users').doc(uid)
+        docRef.get().then(doc => {
+          if (doc.exists) {
+            console.log('user exists')
+            docRef.set({
+              optIn: optIn,
+              urlIds: dbArrayUnion(urlId)
+            }, { merge: true })
+          } else {
+            console.log('user dont exists')
+            docRef.set({
+              urlIds: [urlId],
+              email: userEmail,
+              optIn: optin
+            })
           }
-          fbAuth.signInWithEmailLink(email, window.location.href)
-            .then(result => {
-              console.log('signed in', result.user)
-              uid = result.user.uid
-              userEmail = result.user.email
-              console.log(uid, userEmail)
-              dbUpdate()
+          // get urlids for render **not working for some reason. maybe refactor to async function?
+          let urlsGet
+          db.collection('users').doc(uid).get()
+            .then(doc => {
+              urlsGet = doc.data().urlIds
+              console.log(urlsGet)
             })
-            .catch(error => {
-              console.log('signin with email error', error)
+            .then(doc => {
+              setScenarios(urlsGet)
+              console.log(scenarios)
             })
+        })
+        // add user id to scenario  
+        //   db.collection('scenarios').doc(scenarioId).update({
+        //     "owner": uid,
+        //     "private": true,
+        //   })
+        //     .then(() => {
+        //       console.log('scenario uptdated')
+        //     })
+        //     .catch(error => {
+        //       console.log('error', error)
+        //     }) 
+      }
+      // firebase authentication
+
+      const userProcess = async () => {
+        user = await fbAuth.currentUser
+        if (user) {
+          console.log('user already signed in', user)
+          uid = user.uid
+          userEmail = user.email
+          console.log(userEmail)
+          dbUpdate()
+        } else {
+          // if no user proceed with email link validation *
+          console.log('no user')
+          if (fbAuth.isSignInWithEmailLink(window.location.href)) {
+            let email = window.localStorage.getItem('emailForSignIn')
+            if (!email) {
+              email = window.prompt('please provide email for account confirmation')
+            }
+            fbAuth.signInWithEmailLink(email, window.location.href)
+              .then(result => {
+                console.log('signed in', result.user)
+                uid = result.user.uid
+                userEmail = result.user.email
+                console.log(uid, userEmail)
+                dbUpdate()
+              })
+              .catch(error => {
+                console.log('signin with email error', error)
+              })
+          }
         }
       }
+      userProcess()
+      console.log(scenarios)
     }
-    userProcess()
-    console.log(scenarios)
-    }
-   
+
   }, [])
 
   const resultsRows = () => {
@@ -203,7 +204,7 @@ const portal = props => {
             {scenarios.map(item => (
               <ListItem key={item}>
                 <Link href={{ pathname: '/results', query: { urlid: item } }}>
-                {`${prodUrl}/results?urlid=${item}`}
+                  {`${prodUrl}/results?urlid=${item}`}
                 </Link>
               </ListItem>
             ))}
