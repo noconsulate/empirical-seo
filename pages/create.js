5fb1448e29f8799a925d0e2e378879d4d692b2b3
Janssen Kuhn <noconsulate@gmail.com>
2020-03-04 15:08:00 -0700

    moved some stuff around or something i don't know fixed a bug too yeah that's right

--- pages/create.js
+++ pages/create.js
@@ -20,6 +20,7 @@ const useStyles = makeStyles(theme => ({
   },
   description: {
     backgroundColor: 'orange',
+    direction: 'comlumn'
   },
   draft: {
     border: 'solid'
@@ -232,16 +233,18 @@ const Create = props => {
             Thank you!
         </Typography>
           <Typography varaint='body1'>
-            Please follow the link we just emailed to you. Austin needs to write copy for this part.
-  
-            Here is the link to your new survey for you to keep and share.
+            Please follow the link we just emailed to you. Every survey you create that you choose to make private will be associated with your email address, which you can use anytime to see a list of your surveys and private results by clicking on the profile icon in the upper right.
+        </Typography>
+          <Typography variant='body1'>
+            Here is a link to the survey you just created. Be sure to bookmark it for later!
         </Typography>
           <Link href={{ pathname: '/survey', query: { urlid: urlId } }}>
             {`${prodUrl}/results?urlid=${urlId}`}
           </Link>
+          <br />
           <Button onClick={handleReset}>
             Create a new scenario
-        </Button>
+          </Button>
         </div>
       </>
     )
