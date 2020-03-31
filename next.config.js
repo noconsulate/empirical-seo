36a1d547c375db07daed63b39d18ad7f823a0ad1
Janssen Kuhn <noconsulate@gmail.com>
2020-03-31 09:04:20 -0600

    conditional domain env variable cleaned up

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 
-if (process.env.NODE_ENV == 'development') {
-  console.log('development')
   module.exports = {
     env: {
-      domain: 'http://localhost:3000'
+      domain: process.env.NODE_ENV == 'development' ?
+      'http://localhost:3000' :
+      'https://empirical-seo.noconsulate.now.sh'
     }
   };  
-} else {
-  console.log('production')
-  module.exports = {
-    env: {
-      domain: 'https://empirical-seo.noconsulate.now.sh'
-    }
-  };
-  
-}
