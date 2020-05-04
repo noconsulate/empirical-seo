aab5e748ac90c7ad49346b29f11807cc806a6ea9
Janssen Kuhn <noconsulate@gmail.com>
2020-05-04 12:28:05 -0600

    restoring lost changes to layout

--- components/create/Draft.js
+++ components/create/Draft.js
@@ -1,4 +1,4 @@
-import { Typography, Grid, Button, Paper } from '@material-ui/core'
+import { Typography, Grid, Button, Divider, Paper } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { connect } from 'react-redux'
 
@@ -8,7 +8,7 @@ import SurveyForm from '../survey/SurveyForm'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    paddingTop: theme.spacing(1),
+  //  paddingTop: theme.spacing(1),
   },
   description: {
     //   border: 'solid'
@@ -18,11 +18,19 @@ const useStyles = makeStyles(theme => ({
     //    margin: theme.spacing(1),
   },
   draft: {
- //   border: 'solid',
-    margin: theme.spacing(1),
+    // border: 'solid',
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(2),
+    padding: theme.spacing(.5)
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+  divider: {
+    marginTop: theme.spacing(.8),
   },
   buttons: {
-    paddingTop: theme.spacing(1)
+    //  paddingTop: theme.spacing(1)
   }
 }))
 
@@ -34,7 +42,7 @@ const handleSubmit = (event, text) => {
 const Draft = props => {
   const classes = useStyles()
   const { isUser, userEmail } = props.user
-  const { handlePublish, handlePublishIsUser, handleGoBack, scenarioText } = props
+  const { handlePublish, handlePublishIsUser, handleGoBack, scenarioText, removeUser } = props
 
   if (scenarioText === null) {
     return null
@@ -43,63 +51,77 @@ const Draft = props => {
   if (isUser === true) {
     return (
       <div className={classes.root}>
-        <Grid container spacing={1}>
+        <Grid container>
           <Grid item className={classes.description} xs={12}>
-            <Typography varaint='body1'>
+            <Typography variant='h6'>
               Here is what your survey will look like.
           </Typography>
           </Grid>
-          <Grid item className={classes.draft}>
-            <SurveyForm scenario={scenarioText} />
+          <Grid item className={classes.draft} sm={8} xs={12}>
+            <Paper className={classes.paper}>
+              <SurveyForm scenario={scenarioText} />
+            </Paper>
           </Grid>
           <Grid item className={classes.description}>
             <Typography variant='body1'>
               You are logged in as {userEmail}. Your new scenario will automatically be set to private and saved to this email, unless you logout.
-          </Typography>
-            <Grid container className={classes.buttons}>
-              <Button onClick={handlePublishIsUser}>
-                Continue
-              </Button>
-              <Button onClick={() => removeUser()}>
-                Logout
-              </Button>
-              <Button onClick={handleGoBack}>
-                Edit
-              </Button>
-            </Grid>
+            </Typography>
+            <Divider className={classes.divider} />
           </Grid>
+          <Grid container className={classes.buttons}>
+            <Button
+              onClick={handlePublishIsUser}
+              color='primary'
+            >
+              Continue
+              </Button>
+            <Button onClick={() => removeUser()}>
+              Logout
+              </Button>
+            <Button onClick={handleGoBack}>
+              Edit
+              </Button>
+          </Grid>
+
         </Grid>
       </div>
     )
   }
 
   return (
-    <>
+    <div className={classes.root}>
       <Grid container>
         <Grid item className={classes.description} xs={12}>
-            <Typography variant='body1'>This is paper</Typography>
-          <Typography variant='body1'>
+          <Typography variant='h6'>
             Here is what your survey will look like.
           </Typography>
         </Grid>
-        <Grid item className={classes.survey} xs={12}>
-          <SurveyForm scenario={scenarioText} handleSubmit={handleSubmit} />
+        <Grid item className={classes.draft} sm={8} xs={12}>
+          <Paper className={classes.paper}>
+            <SurveyForm scenario={scenarioText} />
+          </Paper>
         </Grid>
         <Grid item className={classes.description}>
           <Typography variant='body1'>
             After you click publish you'll get a link to the scenario for you to share and a link to special page where you can see all of the results. You'll also have the opportunity to make your results private.
           </Typography>
-          <Grid container>
-            <Button onClick={handlePublish}>
-              Continue
-            </Button>
-            <Button onClick={handleGoBack}>
-              Edit
-            </Button>
-          </Grid>
+          <Divider className={classes.divider} />
         </Grid>
+        <Grid container className={classes.buttons}>
+          <Button
+            onClick={handlePublish}
+            variant='contained'
+            color='primary'
+          >
+            Continue
+            </Button>
+          <Button onClick={handleGoBack}>
+            Edit
+            </Button>
+        </Grid>
+
       </Grid>
-    </>
+    </div>
   )
 }
 const mapState = state => ({
